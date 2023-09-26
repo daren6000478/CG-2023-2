a547133414caf3c14fc9975aac6c07409f4c3962
daren6000478 <141768538+daren6000478@users.noreply.github.com>
2023-09-26 14:05:45 -0500

    Add files via upload

--- c02/threejs_ej03/ejercicio cono.js
+++ c02/threejs_ej03/ejercicio cono.js
@@ -1,17 +1,27 @@
 // Variables globales
-var scene, camera, renderer, cone, controls;
+let scene, camera, renderer, cone, controls;
 
-// Inicialización de la escena
 function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+    // Crear una escena
+    scene = new THREE.Scene();
 
-  // Crear la geometría del cono personalizado
-  var geometry = new THREE.Geometry();
-  var radius = 1;
-  var height = 2;
-  var segments = 20;
+    // Crear una cámara
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.z = 5;
 
+    // Crear un renderizador
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('canvas-container').appendChild(renderer.domElement);
+
+    // Crear una geometría personalizada (cono)
+    createCustomCone();
+
+    // Agregar iluminación
+    const light = new THREE.DirectionalLight(0xffffff, 1);
+    light.position.set(1, 1, 1);
+    scene.add(light);
+
+    // Iniciar controles para el usuario
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+}

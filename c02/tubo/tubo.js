e210eee709e842b8524c97c0a63a3b5b3beff363
daren6000478 <141768538+daren6000478@users.noreply.github.com>
2023-10-03 14:38:42 -0500

    Add files via upload

--- c02/tubo/tubo.js
+++ c02/tubo/tubo.js
@@ -1,13 +1,37 @@
-// Paso 2: Creación de la Curva
-const curve = new THREE.CatmullRomCurve3([
-    new THREE.Vector3(-10, 0, 0),
-    new THREE.Vector3(-5, 5, 0),
-    new THREE.Vector3(0, 0, 0),
-    new THREE.Vector3(5, -5, 0),
-    new THREE.Vector3(10, 0, 0),
-], true);
+// Paso 2: Crear una Escena, Cámara y Renderizador
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("canvas").appendChild(renderer.domElement);
 
-// Paso 3: Generación del Tubo
-const tubeRadius = 1; // Radio del tubo
-const tubeSegments = 64; // Número de segmentos
-const tubeGeometry = new THREE.TubeGeometry(curve, tubeSegments, tubeRadius, 8, false);
+// Paso 3: Crear una Geometría Personalizada (Tubo)
+const path = new THREE.CurvePath();
+const curve = new THREE.CatmullRomCurve3([
+  new THREE.Vector3(-2, 0, 0),
+  new THREE.Vector3(-1, 1, 0),
+  new THREE.Vector3(1, 1, 0),
+  new THREE.Vector3(2, 0, 0)
+]);
+path.add(curve);
+
+const tubeGeometry = new THREE.TubeGeometry(path, 64, 0.5, 8, false);
+
+// Paso 5: Crear una Malla (Objeto 3D)
+const customMesh = new THREE.Mesh(tubeGeometry, new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true }));
+scene.add(customMesh);
+
+// Paso 6: Configurar la Cámara y Renderizar
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Agrega controles para rotar el tubo
+  customMesh.rotation.x += 0.01;
+  customMesh.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
